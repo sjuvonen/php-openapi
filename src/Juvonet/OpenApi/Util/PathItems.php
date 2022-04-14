bc6a4f641e109eec3b316d2ef5cf679dfcfe0853
Samu Juvonen <samu.juvonen@gmail.com>
2022-04-14 12:26:37 +0700

    Add helper function for extract ops from path items

---
+++ src/Juvonet/OpenApi/Util/PathItems.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Juvonet\OpenApi\Util;
+
+use Juvonet\OpenApi\Documentation\Operation;
+use Juvonet\OpenApi\Documentation\PathItem;
+
+final class PathItems
+{
+    private function __construct()
+    {
+    }
+
+    public function extractOperations(PathItem|iterable $pathItems): iterable
+    {
+        if ($pathItems instanceof PathItem) {
+            $pathItems = [$pathItems];
+        }
+
+        foreach ($pathItems as $pathItem) {
+            foreach ($pathItem as $operation) {
+                if ($operation instanceof Operation) {
+                    yield $operation;
+                }
+            }
+        }
+    }
+}
