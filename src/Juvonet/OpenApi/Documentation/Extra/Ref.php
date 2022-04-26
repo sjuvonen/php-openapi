919b727df122daba5acae6c93df26a44d54a7c65
Samu Juvonen <samu.juvonen@gmail.com>
2022-04-26 19:29:57 +0700

    Add Ref::with()

--- src/Juvonet/OpenApi/Documentation/Extra/Ref.php
+++ src/Juvonet/OpenApi/Documentation/Extra/Ref.php
@@ -10,6 +10,14 @@ final class Ref
     ) {
     }
 
+    public function with(string $propertyName, mixed $value): Ref
+    {
+        $values = get_object_vars($this);
+        $values[$propertyName] = $value;
+
+        return new Static(...$values);
+    }
+
     /**
      * Produces a hash that can be used for looking up the matching schema.
      */
